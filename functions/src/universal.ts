3580a913e3b870479e5d65b26d50534c6187ca70
Anthony <ajbarry99@gmail.com>
2022-01-05 11:02:39 +1100

    cleanup.

--- functions/src/universal.ts
+++ functions/src/universal.ts
@@ -1,11 +1,9 @@
 import * as functions from 'firebase-functions';
 import * as express from 'express';
+const server = express();
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const universal = require(`${process.cwd()}/dist/hosting/server/main`);
-// const universalEN = universal.app('en');
-// const universalES = universal.app('es');
 
-const server = express();
 server.get('/', (req, res) => {
   res.redirect('/en'); // Default to english.
 });
