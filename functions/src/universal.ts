e455feb7879a57f09709c24507a6c0f704794618
Anthony <ajbarry99@gmail.com>
2022-01-05 21:05:55 +1100

    simplified html output

--- functions/src/universal.ts
+++ functions/src/universal.ts
@@ -1,5 +1,6 @@
 import * as functions from 'firebase-functions';
 import * as express from 'express';
+import { join } from 'path';
 const server = express();
 const locales = ['en', 'es', 'en-AU'];
 
@@ -21,7 +22,10 @@ locales.forEach((locale: string) => {
    * it will render in the default which is English.
    */
   // eslint-disable-next-line @typescript-eslint/no-var-requires
-  server.use(`/${locale}`, require(`${process.cwd()}/dist/hosting/server/${locale}/main`).app(locale));
+  const universal = require(
+    join(process.cwd(), '/dist/hosting/server/', locale, '/main')
+  ).app(locale);
+  server.use(`/${locale}`, universal);
 });
 
 export const ssr = functions.https.onRequest(server);
