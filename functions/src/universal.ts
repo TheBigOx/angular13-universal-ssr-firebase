0ed14322ec0f3c484db9132fbce8e8361b1d7c2f
Anthony <ajbarry99@gmail.com>
2022-01-09 21:07:11 +1100

    tested deeplinks without language path.

--- functions/src/universal.ts
+++ functions/src/universal.ts
@@ -3,8 +3,16 @@ import * as express from 'express';
 import { join } from 'path';
 const server = express();
 // eslint-disable-next-line @typescript-eslint/no-var-requires
+// const cookieParser = require('cookie-parser');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const locales = require(join(process.cwd(), '/dist/locales.json'));
 
+// TODO: Use cookies to redirect to language.
+// server.use(cookieParser());
+
+/**
+ * Redirect any call to root to the language path.
+ */
 server.get('/', (req, res) => {
   const lang = req.acceptsLanguages(locales);
   if (lang) {
@@ -14,12 +22,31 @@ server.get('/', (req, res) => {
   res.redirect('/en'); // Default to english.
 });
 
+/**
+ * Handle deeplinks without a language path.
+ */
+// server.use(/^\/([^/]*)\/(.*)/, (req, res, next) => {
+//   let lang = req.acceptsLanguages(req.params[0]);
+//   if (!lang) {
+//     lang = req.acceptsLanguages(locales);
+//     if (!lang) {
+//       lang = 'en';
+//     }
+//     if (req.params[1]) {
+//       return res.redirect(`/${lang}/${req.params[0]}/${req.params[1]}`);
+//     } else {
+//       return res.redirect(`/${lang}/${req.params[0]}`);
+//     }
+//   }
+//   next();
+// });
+
+/**
+ * Use localized main server otherwise 
+ * it won't render in the language server side, rather
+ * it will render in the default which is English.
+ */
 locales.forEach((locale: string) => {
-  /**
-   * Use localized main server otherwise 
-   * it won't render in the language server side, rather
-   * it will render in the default which is English.
-   */
   // eslint-disable-next-line @typescript-eslint/no-var-requires
   const universal = require(
     join(process.cwd(), '/dist/hosting/server/', locale, '/main')
